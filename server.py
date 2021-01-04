168e2d1a00d74ff1396641f48d9e9d1a45dd5697
Zafer Yıldız <zafryldz@hotmail.com>
2021-01-04 18:40:13 +0300

    front-end initial

--- server.py
+++ server.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def home_page():
-    return "Hello, world!"
+def homePage():
+    today = datetime.today()
+    dayName = today.strftime("%A")
+    return render_template("index.html")
 
+@app.route("/myBills")
+def myBillPage():
+    return render_template("myBill.html")
+
+@app.route("/donate")
+def donationBills():
+    return render_template("donate.html")
+
+@app.route("/bankAccount")
+def bankAccount():
+    return render_template("bankAccount.html")
+
+@app.route("/outage")
+def outage():
+    return render_template("outages.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="8080", debug=True)
